bae6baa17c2794dc25e1211b1bfd8ffe3b503f02
Slavk1ng <59974027+Slavk1ng@users.noreply.github.com>
2020-05-03 13:41:56 -0500

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+  
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+    responseObject.JSON.parse(xhr.responseText);
+    
+    var newContent = '';
+    for (var i = 0; i < responseObject.pageData.length; i++) {
+        newContent += '<div class="desc">';
+        newContent += data.pageData[i].desc;
+        newContent += '</div>';
+    }
+    
+}
+ 
+xhr.open('GET', "https://github.com/thekaysev/final/blob/master/data/final.JSON", true);
+xhr.send(null);
+
