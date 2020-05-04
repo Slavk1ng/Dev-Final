eeb86ebdd2936b894e57075f7a0ea98541ea70d0
Slavk1ng <59974027+Slavk1ng@users.noreply.github.com>
2020-05-04 04:33:55 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -36,6 +36,7 @@ xhr.onload = function() {
                 newContent += '<li>' + responseObject.pageData[i].DIS3 + '</li>';
 		newContent += '</ul>';
 		newContent += '<b>' + responseObject.pageData[i].DIS4 + '</b>';
+	        newContent += '<li>' + responseObject.pageData[i].trtwo + '</li>';
         newContent += '</div>';
     }
 	//update the page with the new content 
