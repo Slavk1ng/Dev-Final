1aed4ade2d2fdfb14e9c2ba96228a2e4e47ff65e
Slavk1ng <59974027+Slavk1ng@users.noreply.github.com>
2020-05-04 03:38:52 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -37,13 +37,15 @@ xhr.onload = function() {
 		newContent += '<li>' + responseObject.pageData[i].DIS3 + '</li>';
 	        newContent += '</ul>';
 		newContent += '<u>' + responseObject.pageData[i].DIS4 + '</u>';
-	    newContent += '</div>'
+	    newContent += '<li>' + responseObject.pageData[i].trone + '</li>';
+		newContent += '<li>' + responseObject.pageData[i].trtwo + '</li>';
+		newContent += '</ul>';
+        newContent += '</div>';
     }
 	//update the page with the new content 
 	document.getElementById('hermes').innerHTML = newContent;
     
 };
- 
  //prepare the request
 xhr.open('GET', "https://slavk1ng.github.io/Dev-Final/data/final.json", true);
 //send the request
